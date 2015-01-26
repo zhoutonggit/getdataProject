7b2e4d8102b68f37561e78a33533dd6de23b2646
Joe Tonggit config --global user.name Joe <zhoutong@acadiau.ca>
2015-01-25 20:19:57 -0400

    Project for get and clean data on Coursera

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,42 @@
 run_analysis <- function(){
-    # all the datasets are in the same directory as default
+    ## all the datasets are in the same directory as default
     
-    # read the data into each variables
+    ## read the data into each variables
     trainX <- read.table("./Dataset/train/X_train.txt")
     trainY <- read.table("./Dataset/train/y_train.txt")
     features <- read.table("./Dataset/features.txt")
     testY <- read.table("./Dataset/test/y_test.txt")
     testX <- read.table("./Dataset/test/X_test.txt")
     
-    # combine train dataset and test dataset and then merge two data frames together
+    ## combine train dataset and test dataset and then merge two data frames together
+    train <- cbind(trainX, trainY)
+    test <- cbind(testX, testY)
+    data <- rbind(train, test)
     
+    ## labels the columns with the feature names
+    features <- as.vector(features$V2)
+    colnames(data) <- c(features)
     
+    ## remove duplicated colnames (no mean or std involved)
+    data <- data[,!duplicated(features)]
+    
+    ## extracts the measurements on mean and standard deviation for each measurement
+    ## use dplyr to select the columns and combine to a new data frame
+    library(dplyr)
+    meanData <- select(data, contains("mean()"))
+    stdData <- select(data, contains("std()"))
+    data <- cbind(meanData, stdData)
+    
+    ## label the data set with descriptive varible names
+    acti_labels <- read.table("./Dataset/activity_labels.txt")
+    activity <- rbind(trainY, testY)
+    activity <- activity$V1
+    for (i in 1:length(activity)) {
+        index <- activity[i]
+        label <- as.character(acti_labels[index,]$V2)
+        activity[i] <- label
+    }
+    data <- cbind(data, activity)
+    colnames(data)[67] <-"activity"
+    data
 }
